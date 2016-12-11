40bd915f2bed1cf34fee70229efa8b15f7dcbd65
Janne <suom.jann@gmail.com>
2016-12-11 11:18:35 +0200

    Index'n pohjustusta

---
+++ src/main/java/insta/contr/DefaultController.java
@@ -0,0 +1,15 @@
+package insta.contr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("*")
+public class DefaultController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model) {
+        return "index";
+    }
+}
