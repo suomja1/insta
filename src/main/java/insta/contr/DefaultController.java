21b3539cafa951b54cbed2cd315ef3ccc48dadc8
Janne <suom.jann@gmail.com>
2016-12-16 12:31:05 +0200

    Esimerkkikuvat init()-metodiin

--- src/main/java/insta/contr/DefaultController.java
+++ src/main/java/insta/contr/DefaultController.java
@@ -1,16 +1,22 @@
 package insta.contr;
 
 import insta.dom.Kayttaja;
+import insta.dom.Kuva;
 import insta.dom.Tunniste;
 import insta.repo.KayttajaRepository;
 import insta.repo.KuvaRepository;
 import insta.repo.TunnisteRepository;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +34,8 @@ public class DefaultController {
     private KuvaRepository kuvaRepository;
     
     @PostConstruct
-    public void init() {
+    @Transactional
+    public void init() throws IOException {
         if (kayttajaRepository.findByKayttajanimi("taavetti99") != null) {
             return;
         }
@@ -36,12 +43,26 @@ public class DefaultController {
         Kayttaja kayttaja = new Kayttaja();
         kayttaja.setKayttajanimi("taavetti99");
         kayttaja.setSalasana(passwordEncoder.encode("taavetti99"));
+        Kuva kuva = new Kuva();
+        Path polku = Paths.get("src/main/resources/public/P6101609.JPG");
+        kuva.setSisalto(Files.readAllBytes(polku));
+        kuva.setKuvateksti("Mmhmm...");
+        kuva.setKayttaja(kayttaja);
         kayttaja = kayttajaRepository.save(kayttaja);
+        kayttaja.getKuvat().add(kuva);
+        kuvaRepository.save(kuva);
         
         kayttaja = new Kayttaja();
         kayttaja.setKayttajanimi("_miukumauku_");
         kayttaja.setSalasana(passwordEncoder.encode("sssalasanass"));
+        kuva = new Kuva();
+        polku = Paths.get("src/main/resources/public/P4100070.JPG");
+        kuva.setSisalto(Files.readAllBytes(polku));
+        kuva.setKuvateksti("Raidalla on pitkät viikset.");
+        kuva.setKayttaja(kayttaja);
         kayttaja = kayttajaRepository.save(kayttaja);
+        kayttaja.getKuvat().add(kuva);
+        kuvaRepository.save(kuva);
         
         Tunniste tunniste = new Tunniste();
         tunniste.setNimi("kissakuvat");
@@ -64,9 +85,6 @@ public class DefaultController {
         model.addAttribute("tkayttaja", kayttajaRepository.findByKayttajanimi(kayttajanimi));
         model.addAttribute("kayttajat", kayttajaRepository.findAllByOrderByKayttajanimi());
         model.addAttribute("tunnisteet", tunnisteRepository.findAllByOrderByNimi());
-        
-        System.out.println(kuvaRepository.count());
-        
         return "index";
     }
 }
